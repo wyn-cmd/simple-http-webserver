104f6a4fa581208ffe6c54bffe37a9ccef1eec99
wyn-cmd <80321275+wyn-cmd@users.noreply.github.com>
2021-04-03 16:20:42 +0800

    Update server.py

--- server.py
+++ server.py
@@ -3,8 +3,6 @@ import http.server
 import socketserver
  
 p=8080
-handler=http.server.SimpleHTTPRequestHandler
- 
-with socketserver.TCPServer(('',p),handler)as httpd:
-    print('using port:',p)
-    httpd.serve_forever()
+
+print('using port:',p)
+socketserver.TCPServer(('',p),http.server.SimpleHTTPRequestHandler.serve_forever()
